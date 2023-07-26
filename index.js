47bf624024a0f78e7bcae3f93702da0495107336
Chris L <chris@larsonwerks.com>
2023-07-25 17:07:25 -0700

    added license options

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const questions = [
     {type: 'list',
     name: 'license',
     message: 'What license is being used?',
-    choices:["MIT","Apache","So Forth..."],
+    choices:["MIT","Apache 2.0","GNU v3.0", "BSD 2-Clause Simplified License", "BSD 3-Clause New or Revised License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclispe Public License 2.0", "GNU Affero General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unilicense" ],
     validate: (value)=>{ if(value){return true} else {return "Input a value to continue."}}
     },
     {type: 'input',
